098bb6437c29ffc85885f16a1216bdd315a364cc
Doroteya <doroteya.garbachkova@netinfo.bg>
2023-07-25 21:56:08 +0300

    header buttons

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,59 +4,39 @@
       <div class="container">
         <div class="utf_left_side">
           <div id="logo"> <router-link :to="{name:'home'}"><img src="http://ulisting.utouchdesign.com/ulisting_ltr/images/logo.png" alt=""></router-link> </div>
-          <div class="mmenu-trigger">
-      <button class="hamburger utfbutton_collapse" type="button">
-        <span class="utf_inner_button_box">
-          <span class="utf_inner_section"></span>
-        </span>
-      </button>
-      </div>
-          <div class="clearfix"></div>
         </div>
         <div class="utf_right_side">
-          <div class="header_widget">
+          <div class="header_widget d-flex">
+            <router-link :to="{ name: '' }" class="button border with-icon">
+              <font-awesome-icon :icon="['fas', 'location-dot']" /><span class="txt">{{ $t('text.add_venue') }}</span>
+            </router-link>
             <router-link :to="{ name: 'login' }" class="button border sign-in popup-with-zoom-anim" v-if="!isAuth">
               <font-awesome-icon :icon="['fas', 'sign-in']" />
-              <!-- {{ $t('text.hello') }} --> Вход
+              <span class="txt none">{{ $t('text.login') }}</span>
             </router-link>
-            <!-- <router-link :to="{name:'register'}" class="button border sign-in" v-if="!isAuth"><i class="fa fa-sign-in"></i>Register</router-link>
-            <a class="button border sign-in popup-with-zoom-anim" v-if="isAuth" href="" @click.prevent="logout">
-              <font-awesome-icon :icon="['fas', 'sign-in']" />Logout
-            </a> -->
-            <div>
+            <div v-if="isAuth">
               <div class="dropdown">
-                <button class="button border with-icon dropdown-toggle" type="button" id="addVenue" data-bs-toggle="dropdown" aria-expanded="false">
-                  <font-awesome-icon :icon="['fas', 'location-dot']" /> Добави обект
+                <button class="button-tr dropdown-toggle" type="button" id="profile" data-bs-toggle="dropdown" aria-expanded="false">
+                  <font-awesome-icon :icon="['fas', 'user']" />
                 </button>
-                <ul class="dropdown-menu" aria-labelledby="addVenue">
-                  <li v-for="category in categories" :key="category.id"><router-link :to="{ name: 'createVenue', params: { id: category.id } }">{{ category['category_name_' + $i18n.locale] }}</router-link></li>
+                <ul class="dropdown-menu" aria-labelledby="profile">
+                  <li><a class="dropdown-item" href="#">Настройки</a></li>
+                  <li><a class="dropdown-item" href="#">Разлогване</a></li>
                 </ul>
               </div>
             </div>
-            <!-- <nav id="navigation" class="style_one">
-                        <ul id="responsive">
-                          <li>
-                            <a href="#" class="button border sign-in popup-with-zoom-anim"><font-awesome-icon :icon="['fas', 'location-dot']" /> Добави обект</a>
-                                <ul>
-                                    <li v-for="category in categories" :key="category.id"><router-link :to="{ name: 'createVenue', params: { id: category.id } }">{{ category['category_name_' + $i18n.locale] }}</router-link></li>
-                                </ul>
-                              </li>
-                        </ul>
-                      </nav> -->
             <div>
               <div class="dropdown">
-                <button class="button border with-icon dropdown-toggle" type="button" id="changeLang" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="button-tr dropdown-toggle" type="button" id="changeLang" data-bs-toggle="dropdown" aria-expanded="false">
                   <font-awesome-icon :icon="['fas', 'globe']" />
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="changeLang">
-                  <li><a class="dropdown-item" href="#" @click="changeLanguage('en')">English</a></li>
-                  <li><a class="dropdown-item" href="#" @click="changeLanguage('bg')">Bulgarian</a></li>
+                  <li :class="{'active-lang': currentLanguage === 'en'}"><a class="dropdown-item" href="#" @click="changeLanguage('en')">English</a></li>
+                  <li :class="{'active-lang': currentLanguage === 'bg'}"><a class="dropdown-item" href="#" @click="changeLanguage('bg')">Bulgarian</a></li>
                 </ul>
               </div>
             </div>
         </div>
-          <!-- <a href="#" @click="changeLanguage('en')">English</a>
-          <a href="#" @click="changeLanguage('bg')">Bulgarian</a> -->
         </div>
 
 
@@ -71,7 +51,11 @@ import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'DefaultLayout',
-
+  data() {
+      return {
+          active: false
+      }
+  },
   props: ['categories'],
 
   methods: {
@@ -88,7 +72,10 @@ export default {
   computed: {
     ...mapGetters({
       isAuth: 'auth/authenticated'
-    })
+    }),
+    currentLanguage() {
+        return this.$i18n.locale;
+    }
   },
   mounted() {
 
